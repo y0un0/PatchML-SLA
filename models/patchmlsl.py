12f506b8c1c7f1dbd5073a0570bb5e8c3a2dd63d
y0un0 <younesdriouache@gmail.com>
2024-08-31 22:09:06 +0200

    Implement Attention mechanism + MLP

--- models/patchmlsl.py
+++ models/patchmlsl.py
@@ -12,7 +12,7 @@ HANDLED_ENCODER = ["efficientnet_b0",
                    "efficientnet_b7"]
 
 class PatchMLSL(nn.Module):
-    def __init__(self, model_name="efficientnet_b4", n_blocks=5, embed_dim=256, n_cls=20):
+    def __init__(self, model_name="efficientnet_b4", n_blocks=5, intermediate_dim=128, embed_dim=256, n_cls=20):
         super().__init__()
         # Extracting patch embeddings
         if model_name in HANDLED_ENCODER:
@@ -23,6 +23,7 @@ class PatchMLSL(nn.Module):
         self.codebook = CodebookLabel(n_cls=n_cls, embed_dim=embed_dim)
         # Extracting Image embeddings
         self.cross_attn = PatchMLSLAttention()
+        self.mlp = PatchMLSLMLP(intermediate_dim, embed_dim)
         # TODO: Shared Classifier
 
     def forward(self, patches):
@@ -44,13 +45,27 @@ class CodebookLabel(nn.Module):
 class PatchMLSLAttention(nn.Module):
     def __init__(self):
         super().__init__()
-        pass
-
+    
+    def forward(self, codebook, patch_embed):
+        # Compute Attention weights (Matrix A)
+        attn_weights = torch.matmul(codebook, patch_embed.transpose(1, 2))
+        attn_weights = nn.functional.softmax(attn_weights, dim=-1)
+        # Perform the weighted sum (attn_weights * patch_embed)
+        attn_patch = torch.matmul(attn_weights, patch_embed)
+        return attn_patch
+    
 class PatchMLSLMLP(nn.Module):
-    def __init__(self):
+    def __init__(self, intermediate_dim, embed_dim):
         super().__init__()
-        pass
+        self.fc1 = nn.Linear(embed_dim, intermediate_dim)
+        self.fc2 = nn.Linear(intermediate_dim, embed_dim)
+    
+    def forward(self, attn_patch):
+        hidden_state = self.fc1(attn_patch)
+        hidden_state = nn.functional.relu(hidden_state)
+        hidden_state = self.fc2(hidden_state)
+        return hidden_state
 
 if __name__ == "__main__":
-    model = PatchMLSL(model_name="efficientnet_b4", n_blocks=4, embed_dim=256, n_cls=10)
+    model = PatchMLSL(model_name="efficientnet_b4", n_blocks=4, intermediate_dim=128, embed_dim=256, n_cls=10)
     print(model)
